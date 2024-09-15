9546365beb0ea6d1bd3ac59367367afa5f5cd5d3
Joe Fairburn <joe.fairburn1@gmail.com>
2024-09-15 23:04:34 +0100

    chore: enable ssl on localhost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import tailwind from '@astrojs/tailwind';
 
@@ -7,5 +8,18 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [basicSsl({
+      /** name of certification */
+      name: 'localhost',
+      /** custom trust domains */
+      domains: ['*.localhost'],
+      /** custom certification directory */
+      certDir: './certificates'
+    })],
+    server: {
+      https: true,
+    },
+  },
   integrations: [tailwind(), react()]
 });
